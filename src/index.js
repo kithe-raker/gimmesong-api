dd4ffa4f98f8fd0e956658456eb01546f9dbc2da
kithe-raker <kithe.raker@gmail.com>
2022-10-25 21:33:42 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
+
+const router = require("./api");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const _whitelist = [];
+const _corsOptions = {
+  origin: function (origin, callback) {
+    if (_whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+console.log("NODE_ENV=", process.env.NODE_ENV);
+
+app.use(cors(_corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(router);
+
+app.get("/", (req, res, next) => {
+  res.send(`OK`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
