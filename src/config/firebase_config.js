e3ae889ead9a1c0e1a8fed92650749f04ca88924
kithe-raker <kithe.raker@gmail.com>
2022-11-03 21:50:43 +0700

    edit: add every firebase relevant path

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -13,4 +13,28 @@ if (process.env.NODE_ENV === "production") {
 
 const fs = firebase.firestore();
 
-module.exports = { firebase, fs };
+const pathRef = {
+  // every user relevant path
+  UsersCollection: fs.collection("Users"),
+  UserDocument: function (uid) {
+    if (!uid) throw "no uid provided";
+    return pathRef.UsersCollection.doc(uid);
+  },
+  UserInboxCollection: function (uid) {
+    return pathRef.UserDocument(uid).collection("inbox");
+  },
+  UsernamesCollection: fs.collection("Usernames"),
+  UsernameDocument: function (username) {
+    if (!username) throw "no username provided";
+    return pathRef.UsernamesCollection.doc(username);
+  },
+
+  // song relevant path
+  SongsCollection: fs.collection("Songs"),
+  SongDocument: function (id) {
+    if (!id) throw "no id provided";
+    return pathRef.SongsCollection.doc(id);
+  },
+};
+
+module.exports = { firebase, fs, pathRef };
