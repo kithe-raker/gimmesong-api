4c448de42d24ba85b2b04b5c09d46cee34193482
kithe-raker <kithe.raker@gmail.com>
2022-12-17 16:26:44 +0700

    edit: create `validateVinylStyle` #1

--- src/api/v1/repositories/vinylStyle.repository.js
+++ src/api/v1/repositories/vinylStyle.repository.js
@@ -11,6 +11,32 @@ const methods = {
     const doc = await pathRef.VinylStyle.StyleDocument(type, id).get();
     return { style: doc.data(), exists: doc.exists };
   },
+  /**
+   *
+   * @param {{
+   *            background: string,
+   *            center: string,
+   *        }} vinylStyle
+   * @returns message string if there are any error occurs
+   */
+  validateVinylStyle: async function (vinylStyle) {
+    if (!vinylStyle.background) return "No vinyl background style provided";
+    if (!vinylStyle.center) return "No vinyl center style provided";
+
+    // validate if the provided vinyl style is exists
+    const backgroundStyle = await this.getStyleDetails(
+      "background",
+      vinylStyle.background
+    );
+    if (!backgroundStyle.exists)
+      return "provided background vinyl's style doesn't exist";
+
+    const CenterStyle = await this.getStyleDetails("center", vinylStyle.center);
+    if (!CenterStyle.exists)
+      return "provided center vinyl's style doesn't exist";
+
+    return;
+  },
 };
 
 module.exports = methods;
